2fa5f066124c8b7a1e1ecc703eec70c0ea132cd1
rutgerklamer <rutger.klamer@gmail.com>
2017-05-14 23:04:06 +0200

    Added cubemap support

--- webGL/Texture.js
+++ webGL/Texture.js
@@ -1,9 +1,7 @@
 var boxTexture;
+var skyboxTexture;
 
 function CreateTexture() {
-    //
-    // Create texture
-    //
     boxTexture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, boxTexture);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
@@ -16,4 +14,18 @@ function CreateTexture() {
         document.getElementById('crate-image')
     );
     gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+function CreateSkybox()
+{
+    skyboxTexture = gl.createTexture();
+  	gl.bindTexture(gl.TEXTURE_CUBE_MAP, skyboxTexture);
+  	for (var i = 0; i < 6; i++) {
+  		gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE,document.getElementById('crate-image'));
+  	}
+  	gl.texParameteri( gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR );
+  	gl.texParameteri( gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR );
+  	gl.texParameteri( gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE );
+  	gl.texParameteri( gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE );
+  	gl.bindTexture(gl.TEXTURE_CUBE_MAP, null);
 }
